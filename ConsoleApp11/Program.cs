d5ab6b1aa6a3a4b502b6ef449b7373d9c93fb787
visualstudio <visualstudio@122-PC40>
2019-03-06 11:14:33 -0500

    lab 1

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//student name yogesh yogesh
+// student id c0722784
+//csd3354 Section 2
+//Assignment 2
+// march 6,2019
 
-namespace ConsoleApp11
+namespace DelegatesAndEvents
+{
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
+
+    }
+}
+namespace DelegatesAndEvents
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method2();
         }
     }
 }
